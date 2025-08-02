4ac037deaa70b26790a37d7fb9899d96a32233d7
Salomao S. <souza95salomao@gmail.com>
2025-08-02 00:02:43 +0000

    :constructor: update(utils): can parse board indexes back to xadrez like positions

--- src/com/chess/utils/ChessUtils.java
+++ src/com/chess/utils/ChessUtils.java
@@ -1,5 +1,7 @@
 package com.chess.utils;
 
+import java.util.stream.Collectors;
+
 import com.chess.constants.ChessConstants;
 
 public class ChessUtils {
@@ -11,5 +13,25 @@ public class ChessUtils {
     indexes[1] = ChessConstants.CELS.get(keys[1]);
 
     return indexes;
+  }
+
+  public static String mapBoardIndexesToPosition(int[] indexes) {
+    StringBuilder b = new StringBuilder();
+    var columnKeys = ChessConstants.CELS.keySet().stream().collect(Collectors.toList()).subList(0, 8);
+    var rowKeys = ChessConstants.CELS.keySet().stream().collect(Collectors.toList()).subList(8, 16);
+
+    for (String key : columnKeys) {
+      if (ChessConstants.CELS.get(key).equals(indexes[0])) {
+        b.append(key);
+      }
+    }
+
+    for (String key : rowKeys) {
+      if (ChessConstants.CELS.get(key).equals(indexes[1])) {
+        b.append(key);
+      }
+    }
+
+    return b.toString();
   }
 }
