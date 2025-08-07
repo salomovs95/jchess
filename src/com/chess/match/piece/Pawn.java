6f3750b830ba5c6c882d8d3beef1b11266c66991
Salomao S. <souza95salomao@gmail.com>
2025-08-07 12:32:29 +0000

    :recycle: update(piece/pawn): piece movement improvements

--- src/com/chess/match/piece/Pawn.java
+++ src/com/chess/match/piece/Pawn.java
@@ -1,5 +1,8 @@
 package com.chess.match.piece;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.chess.board.ChessPiece;
 import com.chess.utils.ChessUtils;
 
@@ -10,22 +13,50 @@ public class Pawn extends BoardPiece {
 
   @Override
   public void move(String destination, ChessPiece[][] board) {
-    if (this.validateMove(destination, board)) {
+    var possibleMoves = calculatePossibleMoves(board);
+    System.out.println(possibleMoves);
+
+    if(possibleMoves.contains(destination)) {
       super.move(destination, board);
     }
   }
 
   @Override
-  public boolean validateMove(String destination, ChessPiece[][] board) {
-    int[] destinationVector = ChessUtils.mapPositionToBoardIndexes(destination);
+  public Set<String> calculatePossibleMoves(ChessPiece[][] board) {
     int[] positionVector = ChessUtils.mapPositionToBoardIndexes(position);
 
     int nextRow = color.equals("WHITE") ? positionVector[1]-1 : positionVector[1]+1;
+    Set<String> possiblePositionsSet = new HashSet<>();
 
-    if ((destinationVector[0] == positionVector[0]+1 && destinationVector[1] == nextRow) && board[destinationVector[1]][destinationVector[0]] != null) return true;
-    if ((destinationVector[0] == positionVector[0]+1 && destinationVector[1] == nextRow) && board[destinationVector[1]][destinationVector[0]] != null) return true;
-    if ((destinationVector[0] == positionVector[0] && destinationVector[1] == nextRow) && board[destinationVector[1]][destinationVector[0]] == null) return true;
+    String nextPosition = "";
 
-    return false;
+    if (positionVector[0]-1 > 0 && board[nextRow][positionVector[0]-1] != null) {
+      int[] nextPositionIndexes = { positionVector[0]-1, nextRow };
+      nextPosition = ChessUtils.mapPositionIndexesToBoardPlaces(nextPositionIndexes);
+      possiblePositionsSet.add(nextPosition);
+    }
+
+    if (positionVector[0]+1 < 8 && board[nextRow][positionVector[0]+1] != null) {
+      int[] nextPositionIndexes = { positionVector[0]+1, nextRow };
+      nextPosition = ChessUtils.mapPositionIndexesToBoardPlaces(nextPositionIndexes);
+      possiblePositionsSet.add(nextPosition);
+    }
+
+    if (board[nextRow][positionVector[0]] == null) {
+      int[] nextPositionIndexes = { positionVector[0], nextRow };
+      nextPosition = ChessUtils.mapPositionIndexesToBoardPlaces(nextPositionIndexes);
+      possiblePositionsSet.add(nextPosition);
+    }
+
+    if (ChessConstants.INIT_POSITIONS.get(color + "_" + role).contains(position)) {
+      nextRow = color.equals("WHITE") ? positionVector[1]-2 : positionVector[1]+2;
+      if (board[nextRow][positionVector[0]] == null) {
+        int[] nextPositionIndexes = { positionVector[0], nextRow };
+        nextPosition = ChessUtils.mapPositionIndexesToBoardPlaces(nextPositionIndexes);
+        possiblePositionsSet.add(nextPosition);
+      }
+    }
+
+    return possiblePositionsSet;
   }
 }
