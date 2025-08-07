db1530bf135542e10e9811910bb0eb68975ecf06
Salomao S. <souza95salomao@gmail.com>
2025-08-07 12:26:26 +0000

    :recycle: refactor(chess): refine board positions conversion/parsing

--- src/com/chess/utils/ChessUtils.java
+++ src/com/chess/utils/ChessUtils.java
@@ -9,29 +9,17 @@ public class ChessUtils {
     int[] indexes = new int[2];
     String[] keys = position.split("");
 
-    indexes[0] = ChessConstants.CELS.get(keys[0].toUpperCase());
-    indexes[1] = ChessConstants.CELS.get(keys[1]);
+    indexes[0] = ChessConstants.COL_KEYS.indexOf(keys[0].toUpperCase());
+    indexes[1] = ChessConstants.ROW_KEYS.indexOf(keys[1]);
 
     return indexes;
   }
 
-  public static String mapBoardIndexesToPosition(int[] indexes) {
-    StringBuilder b = new StringBuilder();
-    var columnKeys = ChessConstants.CELS.keySet().stream().collect(Collectors.toList()).subList(0, 8);
-    var rowKeys = ChessConstants.CELS.keySet().stream().collect(Collectors.toList()).subList(8, 16);
+  public static String mapPositionIndexesToBoardPlaces(int[] boardIndexes) {
+    String rawPosition = String.format("%s%s",
+      ChessConstants.COL_KEYS.get(boardIndexes[0]),
+      ChessConstants.ROW_KEYS.get(boardIndexes[1]));
 
-    for (String key : columnKeys) {
-      if (ChessConstants.CELS.get(key).equals(indexes[0])) {
-        b.append(key);
-      }
-    }
-
-    for (String key : rowKeys) {
-      if (ChessConstants.CELS.get(key).equals(indexes[1])) {
-        b.append(key);
-      }
-    }
-
-    return b.toString();
+    return rawPosition;
   }
 }
