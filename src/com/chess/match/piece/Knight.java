8105b2fe03c58d028fc03d8a4e09092651878a98
Salomao S. <souza95salomao@gmail.com>
2025-08-11 00:47:42 +0000

    :recycle: refactor(knight): updates knight piece and implements piece movement

--- src/com/chess/match/piece/Knight.java
+++ src/com/chess/match/piece/Knight.java
@@ -1,8 +1,10 @@
 package com.chess.match.piece;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import com.chess.board.ChessPiece;
+import com.chess.utils.ChessUtils;
 
 public class Knight extends BoardPiece {
   public Knight(String color, String position) {
@@ -21,6 +23,89 @@ public class Knight extends BoardPiece {
 
   @Override
   public Set<String> calculatePossibleMoves(ChessPiece[][] board) {
-    return Set.of();
+    int[] positionVec = ChessUtils.mapPositionToBoardIndexes(getPosition());
+    Set<String> possibleMoves = new HashSet<>();
+
+    if (positionVec[1]-2 >= 0) {
+      if (positionVec[0]-1 >= 0) {
+        int[] destPositionVec = { positionVec[0]-1,positionVec[1]-2 };
+        BoardPiece piece = (BoardPiece) board[destPositionVec[1]][destPositionVec[0]];
+
+        if (piece == null || (piece != null && !getColor().equals(piece.getColor()))) {
+          possibleMoves.add(ChessUtils.mapPositionIndexesToBoardPlaces(destPositionVec));
+        }
+      }
+
+      if (positionVec[0]+1 < 8) {
+        int[] destPositionVec = { positionVec[0]+1,positionVec[1]-2 };
+        BoardPiece piece = (BoardPiece) board[destPositionVec[1]][destPositionVec[0]];
+
+        if (piece == null || (piece != null && !getColor().equals(piece.getColor()))) {
+          possibleMoves.add(ChessUtils.mapPositionIndexesToBoardPlaces(destPositionVec));
+        }
+      }
+    }
+
+    if (positionVec[1]+2 < 8) {
+      if (positionVec[0]-1 >= 0) {
+        int[] destPositionVec = { positionVec[0]-1, positionVec[1]+2 };
+        BoardPiece piece = (BoardPiece) board[destPositionVec[1]][destPositionVec[0]];
+
+        if (piece == null || (piece != null && !getColor().equals(piece.getColor()))) {
+          possibleMoves.add(ChessUtils.mapPositionIndexesToBoardPlaces(destPositionVec));
+        }
+      }
+
+      if (positionVec[0]+1 < 8) {
+        int[] destPositionVec = { positionVec[0]+1,positionVec[1]+2 };
+        BoardPiece piece = (BoardPiece) board[destPositionVec[1]][destPositionVec[0]];
+
+        if (piece == null || (piece != null && !getColor().equals(piece.getColor()))) {
+          possibleMoves.add(ChessUtils.mapPositionIndexesToBoardPlaces(destPositionVec));
+        }
+      }
+    }
+
+    if (positionVec[0]-2 >= 0) {
+      if (positionVec[1]-1 >= 0) {
+        int[] destPositionVec = { positionVec[0]-2,positionVec[1]-1 };
+        BoardPiece piece = (BoardPiece) board[destPositionVec[1]][destPositionVec[0]];
+
+        if (piece == null || (piece != null && !getColor().equals(piece.getColor()))) {
+          possibleMoves.add(ChessUtils.mapPositionIndexesToBoardPlaces(destPositionVec));
+        }
+      }
+
+      if (positionVec[1]+1 < 8) {
+        int[] destPositionVec = { positionVec[0]-2,positionVec[1]+1 };
+        BoardPiece piece = (BoardPiece) board[destPositionVec[1]][destPositionVec[0]];
+
+        if (piece == null || (piece != null && !getColor().equals(piece.getColor()))) {
+          possibleMoves.add(ChessUtils.mapPositionIndexesToBoardPlaces(destPositionVec));
+        }
+      }
+    }
+
+    if (positionVec[0]+2 < 8 ) {
+      if (positionVec[1]-1 >= 0) {
+        int[] destPositionVec = { positionVec[0]+2,positionVec[1]-1 };
+        BoardPiece piece = (BoardPiece) board[destPositionVec[1]][destPositionVec[0]];
+
+        if (piece == null || (piece != null && !getColor().equals(piece.getColor()))) {
+          possibleMoves.add(ChessUtils.mapPositionIndexesToBoardPlaces(destPositionVec));
+        }
+      }
+
+      if (positionVec[1]+1 < 8) {
+        int[] destPositionVec = { positionVec[0]+2,positionVec[1]+1 };
+        BoardPiece piece = (BoardPiece) board[destPositionVec[1]][destPositionVec[0]];
+
+        if (piece == null || (piece != null && !getColor().equals(piece.getColor()))) {
+          possibleMoves.add(ChessUtils.mapPositionIndexesToBoardPlaces(destPositionVec));
+        }
+      }
+    }
+
+    return possibleMoves;
   }
 }
